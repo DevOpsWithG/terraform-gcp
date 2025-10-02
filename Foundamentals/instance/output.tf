37bd281aec6e1dc6df8f9092c57ccc9266af8db3
Ganesh Sagar <sagarganesh511@gmail.com>
2025-10-02 06:42:43 +0000

    modified output file

--- Foundamentals/instance/output.tf
+++ Foundamentals/instance/output.tf
@@ -1,7 +1,14 @@
 output "instance_name" {
     value = google_compute_instance.default.*.name ## * means multiple output
 }
-output "external_ip" {
-    value = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
-  
+output "external_ips" {
+  value = [for inst in google_compute_instance.default : try(inst.network_interface[0].access_config[0].nat_ip, null)]
 }
+### other way to get IP output of all instances 
+#output "external_ips" {
+#  value = google_compute_instance.default[*].network_interface[0].access_config[0].nat_ip
+#}
+### If you want IP for only one then
+#output "external_ip" {
+#  value = google_compute_instance.default[0].network_interface[0].access_config[0].nat_ip
+#}
