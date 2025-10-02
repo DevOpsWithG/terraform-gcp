9b32f4dc8591a753a95ba5615d9f74cdeaf46386
Ganesh Sagar <sagarganesh511@gmail.com>
2025-10-02 14:32:48 +0000

    added bucket terraform file

---
+++ Foundamentals/bucket/main.tf
@@ -0,0 +1,14 @@
+resource "google_storage_bucket" "bucket" {
+    name = "basic-bucket"
+    location = "var.region"
+    storage_class = "REGIONAL"
+
+    labels = {
+      name = "basic-bucket"
+      location = "var.region"
+    }
+
+    versioning {
+      enabled = true
+    }
+}
