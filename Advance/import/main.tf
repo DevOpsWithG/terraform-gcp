1315b536b8427820fd91d32c46ed7539e5db552e
Ganesh Sagar <sagarganesh511@gmail.com>
2025-10-08 13:50:51 +0000

    added import task

--- Advance/import/main.tf
+++ Advance/import/main.tf
@@ -1,3 +1,26 @@
+variable "project"{
+    default = "neat-glazing-473202-f3"
+}
+variable "region" {
+    default = "us-central1"
+}
+variable "zone" {
+    default = "us-central1-a"
+}
+variable "credentials"{
+    default = "/home/sagarganesh511/credentials/sa-token.json"
+}
+
+
+provider "google" {
+    project = var.project
+    region = var.region
+    zone = var.zone
+    credentials = file(var.credentials)
+}
+
+
 resource "google_compute_instance" "existing-vm" {
 
 }
+
