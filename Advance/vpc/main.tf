000e4dbe2c1577cea9e5f1fb43db879eb5cc6f9f
Ganesh Sagar <sagarganesh511@gmail.com>
2025-11-01 09:49:25 +0000

    modified vpc tf files

--- Advance/vpc/main.tf
+++ Advance/vpc/main.tf
@@ -1,17 +1,17 @@
 resource "google_compute_network" "network" {
-  name = my-pvc
+  name = "my-pvc"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "subnet" {
-  name = my-subnet
+  name = "my-subnet"
   network = google_compute_network.network.id
   region = "us-central1"
   ip_cidr_range = "10.0.0.0/24"
 }
 
 resource "google_compute_firewall" "allow-http" {
-  name = allow-http
+  name = "allow-http"
   network = google_compute_network.network.id
   allow {
     protocol = tcp
