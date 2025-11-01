b63511628052f0011522dab6398c690406a356c7
Ganesh Sagar <sagarganesh511@gmail.com>
2025-11-01 09:45:08 +0000

    added vpc tf files

---
+++ Advance/vpc/main.tf
@@ -0,0 +1,21 @@
+resource "google_compute_network" "network" {
+  name = my-pvc
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name = my-subnet
+  network = google_compute_network.network.id
+  region = "us-central1"
+  ip_cidr_range = "10.0.0.0/24"
+}
+
+resource "google_compute_firewall" "allow-http" {
+  name = allow-http
+  network = google_compute_network.network.id
+  allow {
+    protocol = tcp
+    ports = ["80"]    
+  }
+  source_ranges = ["0.0.0.0./0"]
+}
